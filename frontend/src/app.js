a7440073b97e7c8318a18c84f2bdfa5e11b24ad1
Serhat Ozturk <so2turk@yahoo.com>
2022-06-04 17:21:02 +0200

    Create login page

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,13 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
 import Register from './pages/register'
+import Login from './pages/login'
 import './app.css'
 
 function App() {
+	const [user, setUser] = useState()
+
 	return (
 		<Router>
 			<div className="app">
 				<Routes>
 					<Route path="/" element={<Register />} />
+					<Route
+						path="/login"
+						element={<Login user={user} setUser={setUser} />}
+					/>
 				</Routes>
 			</div>
 		</Router>
---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,54 @@
+import { useRef, useState } from 'react'
+import axios from 'axios'
+import JWTDecode from 'jwt-decode'
+import '../app.css'
+
+const Login = ({ user, setUser }) => {
+	const [logSuccess, setLogSuccess] = useState(false)
+	const [logFailure, setLogFailure] = useState(false)
+	const emailRef = useRef()
+	const passwordRef = useRef()
+
+	const handleLog = async (e) => {
+		e.preventDefault()
+
+		const userToLog = {
+			email: emailRef.current.value,
+			password: passwordRef.current.value,
+		}
+
+		try {
+			const res = await axios.post('/api/users/login', userToLog)
+			setUser(JWTDecode(res?.data?.accessToken).name)
+			setLogSuccess(true)
+			setLogFailure(false)
+		} catch (err) {
+			console.log(err)
+			setLogSuccess(false)
+			setLogFailure(true)
+		}
+	}
+
+	return (
+		<div>
+			<form onSubmit={handleLog}>
+				<input type="email" placeholder="E-mail" ref={emailRef} required />
+				<input
+					type="password"
+					placeholder="Password"
+					ref={passwordRef}
+					required
+				/>
+				<button type="submit">Login</button>
+			</form>
+			<div>
+				{logSuccess && (
+					<span className="success">{user}'s login is successful</span>
+				)}
+				{logFailure && <span className="failure">Login is failed</span>}
+			</div>
+		</div>
+	)
+}
+
+export default Login
