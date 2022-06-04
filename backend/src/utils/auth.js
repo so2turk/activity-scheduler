dcdb3c2d945a07da12b9ae8e5536431ecca1df1d
Serhat Ozturk <so2turk@yahoo.com>
2022-06-04 13:34:11 +0200

    Install JWT and create access token for login

---
+++ backend/src/utils/auth.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken'
+const accessTokenKey = process.env.ACCESS_TOKEN_KEY
+
+export const genAccessToken = (date, user) => {
+	return jwt.sign(
+		{
+			date: date,
+			name: user.name,
+			id: user._id,
+		},
+		accessTokenKey,
+		{
+			expiresIn: '1d',
+		}
+	)
+}
